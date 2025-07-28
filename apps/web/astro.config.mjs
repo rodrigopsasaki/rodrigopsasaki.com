c8960733724be8b4aef47d4d7a8c59c13155cb66
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2025-07-28 02:34:34 -0300

    adding website to astro config

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rodrigopsasaki.com',
   integrations: [tailwind()],
   output: 'static',
   trailingSlash: 'never',
